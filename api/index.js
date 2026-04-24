2f4b987a4faf59943e0ca4de52752616fd489f91
Ryzik3489 <ryzik3489@gmail.com>
2026-04-24 13:02:11 +0500

    Implement HTTP proxy server in index.js
    Replace async import with HTTP proxy server setup.

--- api/index.js
+++ api/index.js
@@ -1,3 +1,15 @@
-(async() => {
-  await import('./index.mjs');
-})();
+const http = require('http');
+const httpProxy = require('http-proxy');
+const proxy = httpProxy.createProxyServer({});
+
+module.exports = (req, res) => {
+  // Замени на сайт, который хочешь открыть
+  const target = "https://google.com"; 
+  
+  proxy.web(req, res, {
+    target: target,
+    changeOrigin: true
+  }, (e) => {
+    res.status(500).send(e.message);
+  });
+};
